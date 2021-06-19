5a1894a741217d4a4e3d51145b98164a5024a6b1
learningvoid <68837718+learningvoid@users.noreply.github.com>
2021-06-19 20:07:08 +0530

    New react app
    Todo app

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            Md Faishal (Full Stack Developer)
+            <p> Projects Done By Md Faishal 
+                </p>        </div>
+    )
+}
